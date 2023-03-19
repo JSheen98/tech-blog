dbef854ac56589d5684f224d2942032b29b54300
Jsheen98 <jsheendev@gmail.com>
2023-03-18 18:16:46 -0600

    Added (broken) functionality to the login and signup

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.set('view engine', 'handlebars')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static('public'))
 
 app.use(routes)
 
