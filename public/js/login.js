203f5a3e01bd07b006f45afe4210c6ce0e42c70e
Jsheen98 <jsheendev@gmail.com>
2023-03-19 11:28:14 -0600

    Added fixes for login, signup, and logout issues

--- public/js/login.js
+++ public/js/login.js
@@ -13,38 +13,19 @@ const loginFormHandler = async (event) => {
         })
 
         if (response.ok) {
-            document.location.replace('/dashboard')
+            document.location.replace('/')
         } else {
             alert(response.statusText)
         }
     }
 }
 
-const signupFormHandler = async (event) => {
-    event.preventDefault()
-
-    const username = document.querySelector('#signup-username').value.trim()
-    const password = document.querySelector('#signup-password').value.trim()
-
-    if (username && password) {
-        const response = await fetch('/api/user', {
-            method: 'POST',
-            body: JSON.stringify({ username, password }),
-            headers: { 'Content-Type': 'application/json' },
-        })
-
-        if (response.ok) {
-            document.location.replace('/dashboard')
-        } else {
-            alert(response.statusText)
-        }
-    }
-}
+// if (document.querySelector('.login-form')) {
+//     document
+//     .querySelector('.login-form')
+//     .addEventListener('submit', loginFormHandler)
+// }
 
 document
     .querySelector('.login-form')
     .addEventListener('submit', loginFormHandler)
-
-document
-    .querySelector('.signup-form')
-    .addEventListener('submit', signupFormHandler)
